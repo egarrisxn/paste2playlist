c1c594eeb9b317b9cfa8eeb131ddbe5d7ad1a7a1
egarrisxn <egarrisxn@gmail.com>
2025-12-28 21:12:16 -0500

    small tweak

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-auto flex-col items-center justify-center px-4 text-center sm:flex-row">
-        <h1 className="text-4xl font-bold">paste2playlist</h1>
+        <h1 className="text-4xl font-bold">Paste2Playlist</h1>
       </div>
     </>
   );
