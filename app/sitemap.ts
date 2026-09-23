d0934919e8770b47d5f5a13f72cc3a1ce8a38602
egarrisxn <egarrisxn@gmail.com>
2026-09-23 13:13:28 -0400

    seo

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const SITE_URL = "https://swaybae.net";
+  const SITE_URL = "https://paste2playlist.vercel.app";
 
   const staticRoutes: MetadataRoute.Sitemap = [
     { url: SITE_URL, changeFrequency: "yearly", priority: 0.1 },
